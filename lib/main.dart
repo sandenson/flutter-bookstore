6c1a62c5bfd33f8878d9534916f9c1e805999246
Luiz Gabriel Sandes <lgabrielsandes@gmail.com>
2021-05-03 10:05:53 -0300

    chore: Título da aplicação corrigido.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bookstore/home/home.dart';
-import 'package:flutter_bookstore/shared/widgets/map/map_widget.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      title: 'Flutter Library',
+      title: 'Flutter Bookstore',
       home: HomePage(),
       debugShowCheckedModeBanner: false,
     ),
